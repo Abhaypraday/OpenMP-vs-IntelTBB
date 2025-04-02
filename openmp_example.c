0c1b2e221d272d5a2225e9f8059f8845ffa240f4
Abhaypraday Bhagvanrao Shinde <123622042+Abhaypraday@users.noreply.github.com>
2025-04-02 16:19:00 +0530

    Added openmp example code/implementation

---
+++ openmp_example.c
@@ -0,0 +1,11 @@
+// openmp_example.c
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        printf("Hello from thread %d\n", omp_get_thread_num());
+    }
+    return 0;
+}
